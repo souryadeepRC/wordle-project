ae2ca8e2f526f64e8ffeffae2bc9a66dc321da95
Coder Sourya <thecreativeworldforall@gmail.com>
2022-02-22 22:13:06 +0530

    Modyfying Business Logic in Inde.js and adding data.json file

--- js/index.js
+++ js/index.js
@@ -1,36 +1,139 @@
 
-var WORD_DATA  = []
-const getDictionaryWordCollection = () => {
-    let url = 'data/words.json'
-    $.get(url, (data, status) => { 
-        return data
-    });
+let WORD_DATA  = [] 
+const URL = 'data/data.json'
+
+//const LETTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L','M','N','O','P','Q','R','S', 'T', 'U', 'V', 'W', 'X', 'V', 'Z']
+const LETTER_LIST = ['A', 'B', 'C']
+let MAX_LETTER_COUNT = 5
+const getDictionaryWordCollection = () => { 
+    $.ajax({
+        url: URL,
+        type: 'GET', 
+        success:function(data) {
+            WORD_DATA = data 
+        }
+    }); 
 }
-const loadContent = () => {
-    if(WORD_DATA){
+const filterWithValidWord = () => {
+    getDictionaryWordCollection() 
+    console.log('Loader On..');
+    setTimeout(() => {
         console.log(WORD_DATA);
-        console.log('Loader off...');
-        clearInterval(myInterval); 
-    }else{
-        console.log('Loader On...');
+        console.log('Loader Off..'); 
+    }, 5000); 
+}
+const getWord = (letterDetail, startingLetter, letterCount) => { 
+    let finalWordArray = []
+        //console.log([letterDetail, startingLetter, letterCount]);
+    if (letterCount != MAX_LETTER_COUNT) {
+         
+        letterDetail[letterCount-1].forEach(letter => {
+            finalWordArray.push(...getWord(letterDetail, startingLetter + letter, letterCount + 1))
+        });
+    } else {
+       /*  console.log('ELSE');
+        console.log(letterDetail[letterCount-1]); */
+
+        let possibleWordArray = []
+
+        letterDetail[letterCount-1].forEach(letter => {
+            possibleWordArray.push(startingLetter + letter)
+        });
+        console.log(`After Position ${letterCount} := ${startingLetter}`);
+        console.log(possibleWordArray);
+        return possibleWordArray
+
     }
+    console.log(finalWordArray);
+    return finalWordArray
 }
 
-function myTimer() {
-    console.log('myTimer Called');
-    if(WORD_DATA.length>0){
-        console.log(WORD_DATA);
-        console.log('Loader off...');
-        clearInterval(myInterval); 
-    }else{
-        console.log('Loader On...');
-    }
+const checkCorrectPosition = (word, perfectPosition) => {
+
+    let checkFlag = true
+    perfectPosition.forEach((positionLetter, index) => {
+        if (positionLetter != '') {
+            checkFlag = checkFlag && word[index] == positionLetter
+        }
+    })
+    return checkFlag
 }
- 
-function loadData() {
-    console.log('Hello World !')
+const filterPerfectPositionWord = (possibleWordList, perfectPosition) => {
+    return possibleWordList.filter(word => checkCorrectPosition(word,perfectPosition))
+}
+const getPossibleWord = (letterDetail) => {
+    let possibleWordArray = [] 
+    console.log(letterDetail);
+    possibleWordArray = getWord(letterDetail, '', 1)
+    console.log(possibleWordArray);
+    return possibleWordArray
+}
+
+const removeExtraLetter = (letterlist, removalLetterDetail, igonreLetterDetail) => {
+
+/*     console.log(removalLetterDetail);
+    console.log(igonreLetterDetail); 
+    let data = letterlist.filter(letter => !removalLetterDetail.find(item => item == letter) )
+
+    console.log(data);
+    let data1 = data.filter(letter => !igonreLetterDetail.find(item => item == letter) )
+    console.log(data1);
+    console.log(letterlist.filter(letter => !removalLetterDetail.find(item => item == letter) && !igonreLetterDetail.find(item => item == letter)));
+    console.log('----------------'); */
+    return letterlist.filter(letter => !removalLetterDetail.find(item => item == letter) && !igonreLetterDetail.find(item => item == letter))
+    /* return (letterlist.filter(letter => !removalLetterDetail.find(item => item == letter)
+        .filter(letter => !igonreLetterDetail.find(item => item == letter)) )) */
+}
+
+const getAttemptLetterDetail = (completeIgnoreLetter, removalLetterAtPosition) => {  
+    let letterList = [] 
+    for (let index = 0; index < MAX_LETTER_COUNT; index++) {
+        letterList.push(removeExtraLetter(LETTER_LIST, completeIgnoreLetter, removalLetterAtPosition[index]))
+    } 
+    return letterList 
+}
+
+const stringPermutations = str => {
+    if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
+    return str
+      .split('')
+      .reduce(
+        (acc, letter, i) =>
+          acc.concat(stringPermutations(str.slice(0, i) + str.slice(i + 1)).map(val => letter + val)),
+        []
+      );
+  };
+  
+
+function loadData() { 
    
-    WORD_DATA = getDictionaryWordCollection()
+  console.log(stringPermutations('abcd'));
+    let completeIgnoreLetter = ['F', 'A', 'I', 'S', 'G']
+    
+    let removalLetterAtPosition = [ 
+        ['R'], // Position 1
+        ['O'], // Position 2
+        [], // Position 3
+        ['N', 'T'], // Position 4
+        ['T'], // Position 5
+    ]
+    let perfectPosition = [
+        '', // Position 1
+        'H', // Position 2
+        'O', // Position 3
+        '', // Position 4
+        '', // Position 5
+    ]
+    //let letterDetail = getAttemptLetterDetail (completeIgnoreLetter, removalLetterAtPosition) 
+    //console.log(letterDetail);
+    //let possibleWordList = getPossibleWord(letterDetail) 
+    
      
-    console.log(WORD_DATA);
+    
+    //let filteredWordArray = filterPerfectPositionWord(possibleWordList, perfectPosition) 
+    //let filteredWordArray filterPerfect Positionword(['FEAST', 'BHPOER",";UJOKL, THORN", "GHOBR", "APPLE"],perfectPosition)
+    
+ 
+    
+    //console.log(filteredWordArray)
 }
