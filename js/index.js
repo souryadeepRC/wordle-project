7cd3c51b3736d35d053800c57846a4eaf1e5065e
Coder Sourya <thecreativeworldforall@gmail.com>
2022-02-22 20:10:16 +0530

    Creating Dictionary Word File

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
+
+var WORD_DATA  = []
+const getDictionaryWordCollection = () => {
+    let url = 'data/words.json'
+    $.get(url, (data, status) => { 
+        return data
+    });
+}
+const loadContent = () => {
+    if(WORD_DATA){
+        console.log(WORD_DATA);
+        console.log('Loader off...');
+        clearInterval(myInterval); 
+    }else{
+        console.log('Loader On...');
+    }
+}
+
+function myTimer() {
+    console.log('myTimer Called');
+    if(WORD_DATA.length>0){
+        console.log(WORD_DATA);
+        console.log('Loader off...');
+        clearInterval(myInterval); 
+    }else{
+        console.log('Loader On...');
+    }
+}
+ 
 function loadData() {
     console.log('Hello World !')
-    console.log(POSSIBLE_WORD);
+   
+    WORD_DATA = getDictionaryWordCollection()
+     
+    console.log(WORD_DATA);
 }
