e810587399c9ac7ad3021f0b4f22328924f12242
Coder Sourya <thecreativeworldforall@gmail.com>
2022-02-23 02:46:41 +0530

    Updatiing Script JS file to add combination logic

---
+++ data/config.js
@@ -0,0 +1,33 @@
+var LETTER_DETAIL = [
+    { 'Letter': 'A', 'Status': false },
+    { 'Letter': 'B', 'Status': false },
+    { 'Letter': 'C', 'Status': false },
+    { 'Letter': 'D', 'Status': false },
+    { 'Letter': 'E', 'Status': false },
+    { 'Letter': 'F', 'Status': false },
+    { 'Letter': 'G', 'Status': false },
+    { 'Letter': 'H', 'Status': false },
+    { 'Letter': 'I', 'Status': false },
+    { 'Letter': 'J', 'Status': false },
+    { 'Letter': 'K', 'Status': false },
+    { 'Letter': 'L', 'Status': false },
+    { 'Letter': 'M', 'Status': false },
+    { 'Letter': 'N', 'Status': false },
+    { 'Letter': 'O', 'Status': false },
+    { 'Letter': 'P', 'Status': false },
+    { 'Letter': 'Q', 'Status': false },
+    { 'Letter': 'R', 'Status': false },
+    { 'Letter': 'S', 'Status': false },
+    { 'Letter': 'T', 'Status': false },
+    { 'Letter': 'U', 'Status': false },
+    { 'Letter': 'V', 'Status': false },
+    { 'Letter': 'W', 'Status': false },
+    { 'Letter': 'X', 'Status': false },
+    { 'Letter': 'V', 'Status': false },
+    { 'Letter': 'Z', 'Status': false }
+]
+
+const LETTER_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'V', 'Z'
+]
+let MAX_LETTER_COUNT = 5
